4dfa8bc73959f3cdc7f091f7c7fa4fdfa4df3628
wᴀsᴍɪ ⊰ <dossariwa@gmail.com>
2023-05-23 02:05:35 +0300

    ⚙ Wrappering the Application to use Cairo font in Arabic.

---
+++ src/components/Wrapper.vue
@@ -0,0 +1,30 @@
+<template>
+  <div :class="appClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, watch } from "vue";
+import { useI18n } from "vue-i18n";
+
+export default {
+  name: "AppWrapper",
+  setup() {
+    const { locale } = useI18n();
+    const appClass = ref("");
+
+    // Update the appClass value based on the selected locale
+    const updateAppClass = () => {
+      appClass.value = locale.value === "ar" ? "font-cairo" : "";
+    };
+
+    onMounted(updateAppClass);
+    watch(locale, updateAppClass);
+
+    return {
+      appClass,
+    };
+  },
+};
+</script>
